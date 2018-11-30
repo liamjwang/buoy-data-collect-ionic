e59bf9a7ac8c3cd8358a085e1c45326872c67bb2
wangl5 <wangl5@users.noreply.github.com>
2018-11-30 09:41:29 -0800

    Remove unused styles and parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {WaterwandBleApiProvider} from "../providers/waterwand-ble-api/waterwand-
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, bleapi: WaterwandBleApiProvider) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
