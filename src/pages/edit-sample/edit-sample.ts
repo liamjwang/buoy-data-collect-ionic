b0a35bfd8fa0fcd6508b07e64b84f613fbdc0080
wangl5 <wangl5@users.noreply.github.com>
2018-11-29 17:38:03 -0800

    Add database access to edit-sample

--- src/pages/edit-sample/edit-sample.ts
+++ src/pages/edit-sample/edit-sample.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {DataManagerProvider} from "../../providers/data-manager/data-manager";
 import {NavParams} from "ionic-angular";
@@ -11,10 +11,41 @@ import {NavParams} from "ionic-angular";
 export class EditSamplePage {
 
   sampleID: number;
-  datestring: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public dataManager: DataManagerProvider) {
+  name: string;
+  description: string;
+  salinity: number;
+  turbidity: number;
+  ph: number;
+  temperature: number;
+  timestamp: string;
+  latitude: number;
+  longitude: number;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private zone: NgZone,
+              private dataManager: DataManagerProvider) {
     this.sampleID = this.navParams.get("sampleID");
+    this.openSample();
+  }
+
+  openSample(): Promise<any> {
+    return this.dataManager.getSampleByID(this.sampleID).then(sample => {
+      console.log("[EditSample] Info: Got sample: "+JSON.stringify(sample));
+      this.zone.run(() => {
+        this.name = sample.name;
+        this.description = sample.description;
+        this.salinity = sample.salinity;
+        this.turbidity = sample.turbidity;
+        this.ph = sample.ph;
+        this.temperature = sample.temperature;
+        let timestampDate = new Date(sample.timestamp);
+        this.timestamp = timestampDate.toLocaleDateString()+"  "+timestampDate.toLocaleTimeString();
+        this.latitude = sample.latitude;
+        this.longitude = sample.longitude;
+      });
+    })
   }
 
   deleteSample() {
