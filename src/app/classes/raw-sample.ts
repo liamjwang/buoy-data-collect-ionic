b807e3a8e876a9cf42108906b27b93ba08da04aa
wangl5 <wangl5@users.noreply.github.com>
2018-12-04 16:56:00 -0800

    Add string formatting to RawSample class

--- src/app/classes/raw-sample.ts
+++ src/app/classes/raw-sample.ts
@@ -1,4 +1,4 @@
-export class RawSample {
+export class RawSample { // TODO: Move unit conversions from the liveview page into this class
   constructor(salinity: number, turbidity: number, ph: number, temperature: number) {
     this.salinity = salinity;
     this.turbidity = turbidity;
@@ -9,4 +9,25 @@ export class RawSample {
   turbidity: number;
   ph: number;
   temperature: number;
+
+  salinityString: string;
+  turbidityString: string;
+  phString: string;
+  temperatureString: string;
+
+  public generateStrings() {
+    this.salinityString = this.salinity.toFixed(1);
+    this.turbidityString = this.turbidity.toFixed(0);
+    this.phString = this.ph.toFixed(1);
+    this.temperatureString = this.temperature.toFixed(1);
+  }
 }
+
+// export class SensorReading {
+//   value: number;
+//   fractionDigits: number;
+//   public toString(): string {
+//     return this.value.toFixed(this.fractionDigits)
+//   }
+//   public getCharacterCount()
+// }
